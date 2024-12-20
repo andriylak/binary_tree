3dc907471382ce9ef76deedaca4a6609424e1ef7
lakatoa <lakatoa@natur.cuni.cz>
2024-12-21 00:56:44 +0100

    BinaryTree.py: added function getListsOfNodesInTheLayer(tree), modified function TraversDepth; DFSandBFS.py: fixed errors in DFS

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,49 +1,64 @@
-class BinaryTree():
+class BinaryTree:
     def __init__(self, value, left_child=None, right_child=None):
         self.value = value
         self.left_child = left_child
         self.right_child = right_child
 
-def traverz(tree):
+
+def TraversPreOrder(tree):
     if tree != None:
         print(tree.value)
-        traverz(tree.left_child)
-        traverz(tree.right_child)
-        
-def TraverzInOrder(tree):
+        TraversPreOrder(tree.left_child)
+        TraversPreOrder(tree.right_child)
+
+
+def TraversInOrder(tree):
     if tree != None:
-        TraverzInOrder(tree.left_child)
+        TraversInOrder(tree.left_child)
         print(tree.value)
-        TraverzInOrder(tree.right_child)
+        TraversInOrder(tree.right_child)
 
-def TraverzSum(tree):
+
+def TraversPostOrder(tree):
     if tree != None:
-        return TraverzSum(tree.left_child) + TraverzSum(tree.right_child)
-    else:
-        return 0
+        TraversPostOrder(tree.left_child)
+        TraversPostOrder(tree.right_child)
+        print(tree.value)
 
-def TraverzDepth(tree):
+
+def TraversSum(tree):
     if tree != None:
-        return max(TraverzDepth(tree.left_child), TraverzDepth(tree.right_child)) + 1
+        return TraversSum(tree.left_child) + TraversSum(tree.right_child) + tree.value
     else:
         return 0
 
-def TraverzDepthv2(tree, h = 0):
-    if tree.left_child != None or tree.right_child != None:
-        return max(TraverzDepth(tree.left_child, h + 1), TraverzDepth(tree.right_child, h + 1)) + 1
+
+def TraversDepthv2(tree):
+    if tree != None:
+        if tree.left_child != None:
+            left = TraversDepthv2(tree.left_child)
+        else:
+            return 0
+        if tree.right_child != None:
+            right = TraversDepthv2(tree.right_child)
+        else:
+            return 0
+        return max(left, right) + 1
     else:
         return 0
 
-def TraverzReturnValues(tree, array = []):
+
+def TraversReturnValues(tree, array=[]):
     if tree != None:
         array.append(tree.value)
-        TraverzReturnValues(tree.left_child)
-        TraverzReturnValues(tree.right_child)
+        TraversReturnValues(tree.left_child)
+        TraversReturnValues(tree.right_child)
     return array
 
-def NumberOfEdgesInTheLayer(tree, depth = 0, count = []):
+
+def NumberOfEdgesInTheLayer(tree, depth=0, count=[]):
     if tree != None:
-        if len(count)<=depth:
+        if len(count) <= depth:
             count.append(0)
             count[depth] += 1
         else:
@@ -52,9 +67,27 @@ def NumberOfEdgesInTheLayer(tree, depth = 0, count = []):
         NumberOfEdgesInTheLayer(tree.right_child, depth + 1, count)
     return count
 
-    
-def VerticalBST(tree, depth=0, space = 3):
+
+def getListsOfNodesInTheLayer(tree, depth=0, result=[]):
     if tree != None:
-        VerticalBST(tree.right_child,depth+1)
-        print(depth*space*' ', tree.value, sep='')
-        VerticalBST(tree.left_child, depth+1)
+        if len(result) <= depth:
+            result.append([tree.value])
+        else:
+            result[depth].append(tree.value)
+        getListsOfNodesInTheLayer(tree.left_child, depth + 1, result)
+        getListsOfNodesInTheLayer(tree.right_child, depth + 1, result)
+    return result
+
+
+def VerticalBST(tree, depth=0, space=3):
+    if tree != None:
+        VerticalBST(tree.right_child, depth + 1)
+        print(f"{tree.value:{(depth*space)}d}")
+        VerticalBST(tree.left_child, depth + 1)
+
+
+tree = BinaryTree(
+    10,
+    BinaryTree(5, BinaryTree(1), BinaryTree(7)),
+    BinaryTree(20, None, BinaryTree(30)),
+)
--- DFSandBFS.py
+++ DFSandBFS.py
@@ -1,9 +1,12 @@
 from collections import deque
-class BinaryTree():
+
+
+class BinaryTree:
     def __init__(self, value, left_child=None, right_child=None):
         self.value = value
         self.left_child = left_child
         self.right_child = right_child
+
 
 def DFS(tree):
     stack = []
@@ -12,19 +15,24 @@ def DFS(tree):
         node = stack.pop()
         if node != None:
             print(node.value)
-            stack.append(tree.left_child)
-            stack.append(tree.right_child)
+            stack.append(node.left_child)
+            stack.append(node.right_child)
+
 
 def BFS(tree):
-    fronta = deque([])
-    fronta.append(tree)
-    while len(fronta) > 0:
-        node = fronta.popleft()
+    queue = deque([])
+    queue.append(tree)
+    while len(queue) > 0:
+        node = queue.popleft()
         print(node.value)
         if node.left_child != None:
-            fronta.append(node.left_child)
+            queue.append(node.left_child)
         if node.right_child != None:
-            fronta.append(node.right_child)
-    
-tree = BinaryTree(10, BinaryTree(5, BinaryTree(1), BinaryTree(7)), BinaryTree(20, None, BinaryTree(30)))
-BFS(tree)
+            queue.append(node.right_child)
+
+
+tree = BinaryTree(
+    10,
+    BinaryTree(5, BinaryTree(1), BinaryTree(7)),
+    BinaryTree(20, None, BinaryTree(30)),
+)
